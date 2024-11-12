cb9d4cbe328a95dca86a11fa9a7105e9fbd10285
ignaciodc8 <diazcabreraignacio@gmail.com>
2024-11-12 17:22:03 -0300

    .

--- public/js/app.js
+++ public/js/app.js
@@ -1,6 +1,6 @@
 document.addEventListener("click", (e) => {
     if (e.target.dataset.short) {
-      const url = `http://localhost:5000/${e.target.dataset.short}`;
+      const url = `https://proyectoweb-express-mongodb.onrender.com/`;
   
       navigator.clipboard
         .writeText(url)
